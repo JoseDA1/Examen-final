946d86f1fe5e98d1095f93306a4c18b4a0da8dfe
Jose <jdangaritasa@ufpso.edu.co>
2024-12-13 13:30:57 -0500

    incremento de poder del agente

--- matrix-reloaded/src/entities/Agente.java
+++ matrix-reloaded/src/entities/Agente.java
@@ -1,13 +1,24 @@
 package entities;
 
+// import java.util.ArrayList;
+
 public class Agente extends Entidad {
     private String habilidadEspecial;
     private String misionAsignada;
+    private int nivelDePoder;
+    // private Anomalia anomalia;
+    // private ArrayList anomaliasAsignadas;
 
-    public Agente(int id, String nombre, String habilidadEspecial) {
+    public Agente(int id, String nombre, String habilidadEspecial, int nivelDePoder) {
         super(id, nombre);
         this.habilidadEspecial = habilidadEspecial;
+        this.nivelDePoder = nivelDePoder;
+        // System.out.println("Nivel de agente: " + this.nivelDePoder);
+
     }
+    // public void asignarAnomalia(){
+    //     System.out.println("Lista de anomalias " + anomaliasAsignadas);
+    // }
 
     public String getHabilidadEspecial() {
         return habilidadEspecial;
@@ -16,14 +27,24 @@ public class Agente extends Entidad {
     public void setHabilidadEspecial(String habilidadEspecial) {
         this.habilidadEspecial = habilidadEspecial;
     }
+    public int getNivelDePoder() {
+        return nivelDePoder;
+    }
+
+    public void setNivelDePoder(int nivelDePoder) {
+        this.nivelDePoder = nivelDePoder;
+    }
 
     public String getMisionAsignada() {
         return misionAsignada;
     }
-
-    public void asignarMision(String mision) {
-        this.misionAsignada = mision;
+    public void incrementarNivel(){
+        nivelDePoder *= nivelDePoder;
     }
+
+    // public void asignarMision(String mision) {
+    //     this.misionAsignada = mision;
+    // }
 
     @Override
     public String toString() {
--- matrix-reloaded/src/management/AgenteManager.java
+++ matrix-reloaded/src/management/AgenteManager.java
@@ -18,8 +18,9 @@ public class AgenteManager {
 
         String nombre = ConsoleUtils.leerTexto("Ingrese el nombre del agente: ");
         String habilidad = ConsoleUtils.leerTexto("Ingrese la habilidad especial del agente: ");
+        int nivelDePoder = (int) ConsoleUtils.leerEntero("Ingrese el nivel de poder del agente: ");
 
-        Agente nuevoAgente = new Agente(id, nombre, habilidad);
+        Agente nuevoAgente = new Agente(id, nombre, habilidad, nivelDePoder);
         agentes.add(nuevoAgente);
         System.out.println("Agente registrado con éxito.");
     }
@@ -31,7 +32,19 @@ public class AgenteManager {
         }
         System.out.println("=== Lista de Agentes ===");
         for (Agente agente : agentes) {
-            System.out.println(agente);
+            System.out.println(agente + ", Nivel: " + agente.getNivelDePoder());
+        }
+    }
+    public void aumentarNivel(){
+        if (agentes.isEmpty()) {
+            System.out.println("No hay agentes registrados.");
+            return;
+        }
+        System.out.println("=== Lista de Agentes ===");
+        for (Agente agente : agentes) {
+            agente.incrementarNivel();
+            System.out.println("Nivel incrementado de el agentes");
+            System.out.println(agente + " Nivel: " + agente.getNivelDePoder());
         }
     }
 
@@ -41,16 +54,23 @@ public class AgenteManager {
             System.out.println("\n=== Menú Agentes ===");
             System.out.println("1. Registrar Agente");
             System.out.println("2. Mostrar Agentes");
-            System.out.println("3. Salir");
+            System.out.println("3. Aumentar nivel");
+            System.out.println("4. Salir");
+            // System.out.println("4. Regresar al menú anterior");
             int opcion = ConsoleUtils.leerEntero("Seleccione una opción: ");
             ConsoleUtils.limpiarBuffer(); // Consumir cualquier salto de línea residual
 
             switch (opcion) {
                 case 1 -> registrarAgente();
                 case 2 -> mostrarAgentes();
-                case 3 -> continuar = false;
+                case 3 -> aumentarNivel();
+                case 4 -> continuar = false;
+                // case 4 -> continuar = true;
                 default -> System.out.println("Opción no válida.");
             }
         }
     }
+    // public void asignarAnomalia(){
+        
+    // }
 }
